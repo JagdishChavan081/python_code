6fe2e8d860b8856646c299369c6dcab881c7048b
JAGDISH CHAVAN <54933866+JagdishChavan081@users.noreply.github.com>
2022-07-09 11:13:17 +0530

    Add files via upload

---
+++ concatanation.py
@@ -0,0 +1,22 @@
+#wpp to perform concatanation  and repataion on strings
+
+#concatanation
+
+f_name = 'Jagdish'
+m_name = 'Dilip'
+l_name = 'Chavan'
+
+#method1
+print(f_name,m_name,l_name)
+
+#method2
+print(f_name+' '+m_name+' '+l_name)
+
+#method3
+print(f'hello,{l_name} {f_name} how are you ?')
+
+#method4
+print("hi {} {}, how are you?".format(f_name,l_name))
+
+#repeation
+print(f_name*4)
